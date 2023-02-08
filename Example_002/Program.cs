deb4b9042a73562a6c0aa327c2b678b88fcfa93f
Ignasiya <ignasiya93@gmail.com>
2023-02-08 19:45:57 +0300

    Второй урок по C#

---
+++ Example_002/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите Ваше имя ");
+string username = Console.ReadLine();
+// string - тип данных строка
+// ReadLine - ввод данных
+Console.Write("Привет, ");
+Console.Write(username);
