8230c50076b2766fe6846b9f37c2ff026c28ed88
Ignasiya <ignasiya93@gmail.com>
2023-02-11 19:07:58 +0300

    Семинар задание 1

---
+++ Example_017 HomeWork8/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+Console.Clear();
+Console.Write("Введите любое натуральное число: ");
+int Number = int.Parse(Console.ReadLine());
+while (Number < 2){
+    Console.WriteLine("Вы ввели не натуральное число");
+    Console.Write("Введите любое натуральное число: ");
+    Number = int.Parse(Console.ReadLine());
+}for (int index = 2; index <= Number; index = index + 2){
+    Console.Write($"{index} ");
+}
