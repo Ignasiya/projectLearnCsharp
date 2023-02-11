db6f653f3e95238f04f369d5436786522682ce2f
Ignasiya <ignasiya93@gmail.com>
2023-02-11 15:05:16 +0300

    Семинар задание 2

---
+++ Example_009 Qadra/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()); // Здесь int.Parse конвертирует введенные в целое число
+int sqr = number * number; // number *= number <=> number = number * number
+Console.WriteLine($"Квадрат числа {number} number -> {sqr}({number * number})"); // интерпалция $ - текст
+int sqr1 = Convert.ToInt32(Math.Pow(number, 2)); //Toint32 - в double Math.Pow библиотека
+Console.WriteLine($"Квадрат числа {number} number -> {sqr1}");
---
+++ Example_010 DZ/Program.cs
@@ -0,0 +1,21 @@
+﻿/* 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+a = 25; b = 5 -> да
+a = 2 b = 10 -> нет
+a = 9; b = -3 -> да
+a = -3 b = 9 -> нет */
+
+Console.Clear();
+Console.WriteLine("Введите число, квадрат: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int numberB = int.Parse(Console.ReadLine());
+int sqr = Convert.ToInt32(Math.Pow(numberB, 2));
+if (sqr == numberA)
+{
+    Console.WriteLine($"Квадрат числа {numberB} ({sqr}) равен числу {numberA}");
+}
+else 
+{
+    Console.WriteLine($"Квадрат числа {numberB} ({sqr}) не равен числу {numberA}");
+}
