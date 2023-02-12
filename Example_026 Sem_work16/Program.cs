0422a73705d11aa0c857475aa9e951edfbc60466
Ignasiya <ignasiya93@gmail.com>
2023-02-12 16:43:06 +0300

    Семинар 2

--- Example_026 Sem_work16/Program.cs
+++ Example_026 Sem_work16/Program.cs
@@ -1,18 +1,17 @@
-﻿/* На столе лежат n монеток. Некоторые из них лежат вверх решкой, а некоторые – гербом. Определите минимальное число монеток, которые нужно перевернуть, 
- чтобы все монетки были повернуты вверх одной и той же стороной. */
+﻿/* На столе лежат n монеток. Некоторые из них лежат вверх решкой, а некоторые – гербом. Определите минимальное число монеток, 
+которые нужно перевернуть,  чтобы все монетки были повернуты вверх одной и той же стороной. */
 
 Console.Clear();
-Console.Write("Сколько монеток?");
-int Ntemp = 0;
-int Ptemp = 0;
+Console.Write("Сколько монеток? ");
+int gerb = 0;
 int size_monet = int.Parse(Console.ReadLine());
 int[] a = new int[size_monet];
 
 for (int i = 0; i < size_monet; i++){
     a[i] = new Random().Next(0, 2);
     Console.Write($"{a[i]} ");
-    if (a[i] == 0) Ntemp++;
-    else Ptemp++;
-}
-if (Ntemp > Ptemp) Console.WriteLine($"Минимально{Ntemp}");
-else Console.WriteLine($"Минимально{Ptemp}");
+    if (a[i] == 1) 
+    gerb++;
+}Console.WriteLine();
+if (gerb < size_monet - gerb) Console.WriteLine($"Минимально{gerb}");
+else Console.WriteLine($"Минимально {size_monet - gerb}");
