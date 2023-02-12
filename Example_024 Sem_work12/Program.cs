fb898e642794505e5d3d6f24013182f1bfcca657
Ignasiya <ignasiya93@gmail.com>
2023-02-12 15:26:52 +0300

    Семинар 2

---
+++ Example_024 Sem_work12/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая будет принимать на вход два числа и выводить, 
+является ли второе число кратным первому. Если второе число не кратно числу первому,
+то программа выводит остаток от деления.
+
+34, 5 -> не кратно, остаток 4
+16, 4  -> кратно */
+
+Console.Clear();
+
+Console.Write("Введите любое двухзначное число: ");
+int NumberA = int.Parse(Console.ReadLine());
+Console.Write("Введите любое двухзначное число: ");
+int NumberB = int.Parse(Console.ReadLine());
+if (NumberA % NumberB == 0) Console.WriteLine($"Число {NumberB} кратно числу {NumberA}");
+else Console.WriteLine($"Число {NumberB} не кратно числу {NumberA}, оастаток {NumberA % NumberB}");
---
+++ Example_025 Sem_work14/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+14  ->  нет
+46  ->  нет
+161 ->  да */
+
+Console.Clear();
+
+Console.Write("Введите любое двухзначное число: ");
+int Number = int.Parse(Console.ReadLine());
+if (Number % 7 == 0 && Number % 23 == 0) Console.WriteLine($"Число {Number} кратно числу 7 и 23");
+else Console.WriteLine($"Число {Number} не кратно числу 7 и 23");
