a997df5ec73917bfa853e52bcc29a8777a99598b
Ignasiya <ignasiya93@gmail.com>
2023-03-04 23:10:53 +0300

    Жизнь на планете

--- Example_078 HomeworkDOP/Program.cs
+++ Example_078 HomeworkDOP/Program.cs
@@ -35,15 +35,80 @@ using static System.Console;
 
 // Основное действие
 Clear();
-string[,] planet = LiveDeathPlanet("Введите размеры планеты (высота, ширина) через пробел и нажмите Enter: ");
-PopulatePlanet(planet);
+string[,] planet = {
+    {"", "#", "-", "-", "-"}, 
+    {"-", "-", "#", "#", "-"}, 
+    {"-", "#", "-", "-", "-"}, 
+    {"-", "-", "#", "-", "-"}, 
+    {"-", "-", "-", "#", "-"}
+    };
+// LiveDeathPlanet("Введите размеры планеты (высота, ширина) через пробел и нажмите Enter: ");
+// PopulatePlanet(planet);
 SeePlanet(planet);
 WriteLine();
 Write("Введите количество поколений, нажмите Enter и наблюдаем симуляцию: ");
 int countGenerations = int.Parse(ReadLine());
 int generation = 0;
 while(generation <= countGenerations){
-    
+    Clear();
+    string[,] evolutionCurrent = IncreasePlanet(planet);  
+    for (int i = 1; i < evolutionCurrent.GetLength(0) - 1; i++){
+        for (int j = 1; j < evolutionCurrent.GetLength(1) - 1; j++){
+            int count = Survival(planet, evolutionCurrent, i, j);
+            if (evolutionCurrent [i, j] == "-" && count == 3){
+                planet [i - 1, j - 1] = "#";
+            }else if (evolutionCurrent [i, j] == "#" && (count < 3 || count > 4)){
+                planet [i - 1, j - 1] = "-";
+            }else if (evolutionCurrent [i, j] == "#" && (count >= 3 && count <= 4)){
+                planet [i - 1, j - 1] = "#";
+            }else{
+                planet [i - 1, j - 1] = "-";
+            }
+        }
+    }
+    SeePlanet(planet);
+    await Task.Delay(4000);
+    generation++;
+}
+
+string[,] IncreasePlanet(string[,] planet) // Увеличение планеты
+{
+    string[,] evolution = new string[planet.GetLength(0) + 2, planet.GetLength(1) + 2];
+    // Увидеть соседей
+    for (int i = 0; i < planet.GetLength(0); i++){
+        for (int j = 0; j < planet.GetLength(1); j++){
+            evolution[i + 1, j + 1] = planet[i, j];
+        }
+    }
+    // Бока
+    for (int i = 0; i < planet.GetLength(0); i++){
+        evolution[i + 1, 0] = planet[i, planet.GetLength(1) - 1];
+        evolution[i + 1, evolution.GetLength(1) - 1] = planet[i, 0];
+    }
+    // Вверх и низ
+    for (int i = 0; i < planet.GetLength(1); i++){
+        evolution[0, i + 1] = planet[planet.GetLength(0) - 1, i];
+        evolution[evolution.GetLength(0) - 1, i + 1] = planet[0, i];
+    }
+    // Углы
+    evolution[0, 0] = planet[planet.GetLength(0) - 1, planet.GetLength(1) - 1];
+    evolution[evolution.GetLength(0) - 1, 0] = planet[0, planet.GetLength(1) - 1];
+    evolution[0, evolution.GetLength(1) - 1] = planet[planet.GetLength(0) - 1, 0];
+    evolution[evolution.GetLength(0) - 1, evolution.GetLength(1) - 1] = planet[0, 0];
+    return evolution;
+}
+
+int Survival(string[,] planet, string[,] evolution, int i, int j) // Проверка на жизнь
+{
+    int count = 0;
+    for (int k = i - 1; k <= i + 1; k++){
+        for (int l = j - 1; l <= j + 1; l++){
+            if (evolution[k, l] == "#"){
+                count ++;
+            } 
+        }
+    }
+    return count;
 }
 
 string[,] LiveDeathPlanet(string text) // Задаем размеры планеты
