97cc0aca9a1457add9d99ecfeabf2f370c2a1e71
Ignasiya <ignasiya93@gmail.com>
2023-03-05 19:30:53 +0300

    Семинар 8

---
+++ Example_080 Sem_Work55/Program.cs
@@ -0,0 +1,76 @@
+﻿// **Задача 55:**Задайте двумерный массив.
+// Напишите программу, которая заменяет строки на столбцы.
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+
+// В итоге получается вот такой массив:
+
+// 1 5 8 5
+// 4 9 4 2
+// 7 2 2 6
+// 2 3 4 7
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите количество строк: ");
+int num1 = int.Parse(ReadLine());
+Write("Введите количество столбцов: ");
+int num2 = int.Parse(ReadLine());
+
+int[,] result = GetArray(num1, num2, 1, 100);
+PrintArray(result);
+WriteLine();
+int[,] resultTurn = new int[result.GetLength(0), result.GetLength(1)]; // Проверка на равенство размеров массива
+if (result.GetLength(0) == result.GetLength(1))
+{
+    resultTurn = TurnArray(result, resultTurn);
+    PrintArray(resultTurn);    
+}
+else WriteLine($"Выполнить замену не возможно");
+
+
+int[,] TurnArray(int[,] array, int[,] temp)
+{
+    if (array.GetLength(0) == array.GetLength(1))
+    {
+        for (int i = 0; i < array.GetLength(0); i++){
+            for (int j = 0; j < array.GetLength(1); j++){
+                temp[i, j] = array[j, i];
+            }
+        }
+    }
+    return temp;
+}
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j]} ");
+        }
+        WriteLine();
+    }
+}
---
+++ Example_081 Sem_Work57/Program.cs
@@ -0,0 +1,98 @@
+﻿// /* **Задача 57:**Составить частотный словарь элементов 
+// двумерного массива. Частотный словарь содержит информацию о том,
+//  сколько раз встречается элемент входных данных.
+
+// { 1, 9, 9, 0, 2, 8, 0, 9 }
+
+// 0 встречается 2 раза
+// 1 встречается 1 раз
+// 2 встречается 1 раз
+// 8 встречается 1 раз
+// 9 встречается 3 раза
+
+// 1, 2, 3, 4, 6, 1, 2, 1, 6
+
+// 1 встречается 3 раза
+// 2 встречается 2 раз
+// 3 встречается 1 раз
+// 4 встречается 1 раз
+// 6 встречается 2 раза*\
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите количество строк: ");
+int num1 = int.Parse(ReadLine());
+Write("Введите количество столбцов: ");
+int num2 = int.Parse(ReadLine());
+
+int[,] result = GetArray(num1, num2, 0, 9);
+PrintArray(result);
+WriteLine();
+string[,] count = OutputCount(result);
+PrintText(count);
+
+string[,] OutputCount(int[,] array)
+{
+    string[,] text = new string[,]
+    {
+        {"0", "встречается", " ", "раз"},
+        {"1", "встречается", " ", "раз"},
+        {"2", "встречается", " ", "раз"},
+        {"3", "встречается", " ", "раз"},
+        {"4", "встречается", " ", "раз"},
+        {"5", "встречается", " ", "раз"},
+        {"6", "встречается", " ", "раз"},
+        {"7", "встречается", " ", "раз"},
+        {"8", "встречается", " ", "раз"},
+        {"9", "встречается", " ", "раз"},
+    };        
+    for (int i = 0; i < text.GetLength(0); i++){
+        int count = 0;
+        for (int j = 0; j < array.GetLength(1); j++){
+            for (int k = 0; k < array.GetLength(1); k++){
+                if (i == array[j, k]) count++;
+            }
+        }
+        text[i,2] = Convert.ToString(count);
+    }
+    return text;
+}
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j]} ");
+        }
+        WriteLine();
+    }
+}
+
+void PrintText(string[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j]} ");
+        }
+        WriteLine();
+    }
+}
