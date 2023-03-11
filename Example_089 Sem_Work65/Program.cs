8db1e501b79c7441f254a64ea0dc8ec12efe30e9
Ignasiya <ignasiya93@gmail.com>
2023-03-11 15:04:20 +0300

    Семинар 9

---
+++ Example_089 Sem_Work65/Program.cs
@@ -0,0 +1,23 @@
+﻿// **Задача 65:**Задайте значения M и N.
+//  Напишите программу, которая выведет все
+//   натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5. -> "1, 2, 3, 4, 5"
+// M = 4; N = 8. -> "4, 5, 6, 7, 8"
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите значение M: ");
+int a = int.Parse(ReadLine());
+Write("Введите значение N: ");
+int b = int.Parse(ReadLine());
+
+string NumberFor(int a, int b)
+{
+    if (a <= b) return $"{a} " + NumberFor(a + 1, b);
+    else return String.Empty;
+}
+
+WriteLine($"Все числа в промежутке от {a} до {b} -> {NumberFor(a, b)}");
